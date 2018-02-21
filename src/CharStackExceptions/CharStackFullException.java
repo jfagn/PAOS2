0f7b26944ea0837f563aac6dec00fa0a484647e8
Julien Fagnan <jfagn33@gmail.com>
2018-02-20 21:28:44 -0500

    Task 1 completed

---
+++ src/CharStackExceptions/CharStackFullException.java
@@ -0,0 +1,12 @@
+// Source code for the user defined exception classes:
+
+package CharStackExceptions;
+
+public class CharStackFullException extends Exception
+{
+           public CharStackFullException()
+          {
+                  super ("Char Stack has reached its capacity of CharStack.MAX_SIZE.");
+           }
+}
+
