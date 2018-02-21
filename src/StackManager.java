f67d24674e4e8ac20771b8c234afc534361a4d1b
Julien Fagnan <jfagn33@gmail.com>
2018-02-20 21:38:09 -0500

    Task 1 completed

--- src/StackManager.java
+++ src/StackManager.java
@@ -135,22 +135,6 @@ public class StackManager
            } // class CharStackProber
 } // class StackManager
 
-class BaseThread extends Thread
-{
-        /*
-         * Data members
-        */
-        public static int iNextTID = 1; // Preserves value across all instances
-        protected int iTID;
-        public BaseThread()
-        {
-              this.iTID = iNextTID;
-              iNextTID++;
-        }
-       public BaseThread(int piTID)
-       {
-             this.iTID = piTID;
-       }
-}
+
 
 
